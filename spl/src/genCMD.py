81d698d28c6536d8f4d887926f7f3d93c5035788
vic-bart <vicbart08@gmail.com>
2025-12-13 22:50:46 +1100

    implemented dynamic timeout and early cmd termination

--- spl/src/genCMD.py
+++ spl/src/genCMD.py
@@ -1,49 +1,60 @@
 import sys
+
+# Global constants
+TIMEOUT: dict[int|int] = {} # map size -> timeout in seconds
+TIMEOUT[0] = 60 # <100x100 -> 60 seconds
+TIMEOUT[10000] = 180 # >100x100 -> 3 minutes
+EXE="../../cbs"
+FN_FOLDER="../bench_mark/"
+RS_FOLDER="../result/"
+ML=[
+    "empty-8-8",
+]
+
+# Global variables
+all=[]
+
 def getTimeout(fn_m):
-    i = 0
     height = 0
     width = 0
 
     try:
-        with open(fn_m, 'r') as file:
-            for line in file:
-                if i == 0:
-                    pass
-                elif i == 1:
-                    height = int(line.split()[-1])
-                elif i == 2:
-                    width = int(line.split()[-1])
+        with open(fn_m,'r') as f:
+            for l in f:
+                if l.split()[0] == "height":
+                    height = int(l.split()[1])
+                elif l.split()[0] == "width":
+                    width = int(l.split()[1])
                 else:
-                    return (3*60) if (height*width>1000) else (60)
-                i += 1
+                    continue
     except FileNotFoundError:
         print(f"The file '{fn_m}' does not exist.")
         return None
+    
+    # For example, assume (height * width) = 10000
+    size = (height * width) + 1 # size = 10001
+    x = list(TIMEOUT.keys()) # [0, 10000]
+    x.append(size) # [0, 10000, 10001]
+    x.sort() # In cases where the size is below max(x)
+    return TIMEOUT[x[x.index(size)-1]]
 
-exe="../release/cbs"
-fn_folder="../bench_mark/"
-rs_folder="../result/"
-all=[]
-ml=[
-    "empty-8-8",
-]
-for m in ml:
-    fn_m= f"{fn_folder}{m}/{m}.map"
+for m in ML:
+    fn_m= f"{FN_FOLDER}{m}/{m}.map"
     for i_type in ["random","even"]:
         for i in range(1,26):
-            fn_ins=f"{fn_folder}{m}/{m}-{i_type}-{i}.scen"
+            fn_ins=f"{FN_FOLDER}{m}/{m}-{i_type}-{i}.scen"
             with open(fn_ins) as f:
                 maxlen=len(f.read().strip().split("\n"))
 
             for k in range(1,min(400,maxlen)):
                 cmd=[
-                    exe,
+                    EXE,
                     "-m",fn_m,
                     "-a",fn_ins,
-                    "-o",f"{rs_folder}{m}-{i_type}-scen-{i}-agents-{k}.csv",
-                    "--outputPaths",f"{rs_folder}{m}-{i_type}-scen-{i}-agents-{k}.txt",
+                    "-o",f"{RS_FOLDER}{m}-{i_type}-scen-{i}-agents-{k}.csv",
+                    "--outputPaths",f"{RS_FOLDER}{m}-{i_type}-scen-{i}-agents-{k}.txt",
                     "-k",k,
-                    "-t",60,
+                    "-t",getTimeout(fn_m),
                 ]
                 all.append(cmd)
 
