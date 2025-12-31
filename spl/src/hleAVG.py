455f8b682fef398478130c4825f022d4e494150c
vic-bart <vicbart08@gmail.com>
2025-12-31 18:22:41 +1100

    changes and running batch scripts for all maps, and removing cmd.txt (it's too big)

--- spl/src/hleAVG.py
+++ spl/src/hleAVG.py
@@ -1,11 +1,12 @@
 import sys
 import glob
 # Global constants
-RS_FOLDER="../result/1/"
+RS_FOLDER="../result/37/"
 
 # Global variables
 hle=[]
 
+# ../../cbs -m ../bench_mark/empty-8-8/empty-8-8.map -a ../bench_mark/empty-8-8/empty-8-8-random-1.scen -o ../result/1.csv --outputPaths ../result/1.txt -k 30 -t 60
 # runtime,#high-level expanded,
 # 6.20293,11795,
 
@@ -22,8 +23,13 @@ for fn in glob.glob(path):
         print(f"The file '{fn}' does not exist.")
 
 print(sum(hle) / len(hle))
-# 1  = 11795.0
-# 8  = 11795.0
-# 16 = 11795.0
-# 32 = 11795.0
-# 64 = 11795.0
+# 16 = 63066.083333333336
+# 24 = 63708.89453125
+# 32 = 63927.203125
+# 33 = 63966.328125
+# 34 = 64046.78125
+# 35 = 64115.2421875
+# 36 = 64132.10590277778 (max)
+# 37 = 64123.146875
+# 48 = 63777.99375
+# 64 = 61635.5390625
