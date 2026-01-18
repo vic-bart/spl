17caf0732be1827ed49abd62b440c1d4bdd8e2fa
vic-bart <vicbart08@gmail.com>
2026-01-19 10:24:28 +1100

    WIP

--- spl/src/MAPFTracker.py
+++ spl/src/MAPFTracker.py
@@ -7,7 +7,7 @@ MOTION[(0,1)]="r"
 MOTION[(1,0)]="d"
 MOTION[(0,-1)]="l"
 MOTION[(-1,0)]="u"
-SOLUTION_FN="solution"
+SOLUTION_FN="solution2"
 RESULT_FOLDER="../result/"
 DEBUG_PERIOD=10 # Seconds
 
@@ -16,6 +16,7 @@ time_last_debug=0
 time_start=0
 time_end=0
 debug_count=0
+debug_total=0
 
 
 def get_result() -> list[str]:
@@ -42,9 +43,6 @@ def convert_solution_string(solution:str="", encoded:bool=True) -> str:
             x = int(position[0])-prev_position[0]
             y = int(position[1])-prev_position[1]
             motions.append(MOTION[(x,y)])
-        else:
-            x = int(position[0])-int(position[0])
-            y = int(position[1])-int(position[1])
         prev_position = (int(position[0]),int(position[1]))
     motion = "".join(motions)
     
@@ -53,20 +51,18 @@ def convert_solution_string(solution:str="", encoded:bool=True) -> str:
     
     encoded_motions=[]
     prev_move=None
-    count=1
+    repeated_move=1
     for move in motion:
-        if prev_move is None:
-            prev_move = move
-            continue
-        if move == prev_move:
-            count += 1
-        if move != prev_move:
-            count = "" if count == 1 else count
-            encoded_motions.append(f"{count}{prev_move}")
-            count=1
+        if prev_move is not None:
+            if move == prev_move:
+                repeated_move += 1
+            else: # move != prev_move
+                repeated_move = "" if repeated_move == 1 else repeated_move
+                encoded_motions.append(f"{repeated_move}{prev_move}")
+                repeated_move=1
         prev_move = move
-    count = "" if count == 1 else count
-    encoded_motions.append(f"{count}{prev_move}")
+    repeated_move = "" if repeated_move == 1 else repeated_move
+    encoded_motions.append(f"{repeated_move}{prev_move}")
     encoded_motion = "".join(encoded_motions)
     
     return encoded_motion
@@ -81,7 +77,7 @@ def create_solution_plan(fn:str) -> str:
         for l in f:
             match = pattern.findall(l)
             solutions.append(convert_solution_string(*match))
-    solution = "\\n".join(solutions)
+    solution = "\n".join(solutions)
     return solution
 
 
@@ -92,6 +88,7 @@ is_txt=False
 flip_up_down="FALSE"
 
 time_start = time.time()
+debug_total = len(result)
 
 try:
     for fn in result: # ../result/Berlin_1_256-even-scen-1-agents-2.txt
@@ -111,7 +108,7 @@ try:
             pass
         else:
             time_last_debug = time.time()
-            print(f"ETA: {(((time_end-time_start)/debug_count)*(len(result)-debug_count))//60} minutes remaining. {debug_count}/{len(result)} solutions formatted.")
+            print(f"ETA: {(((time_end-time_start)/debug_count)*(debug_total-debug_count))//60} minutes remaining. {debug_count}/{debug_total} solutions formatted.")
 
 except:
     pass
