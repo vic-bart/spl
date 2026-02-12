613db24c6b0e22c31e0bc25e7e988ea5dbd32a98
vic-bart <vicbart08@gmail.com>
2026-02-12 11:40:37 +1100

    finished genScen for empty-3-3

--- spl/src/genScen.py
+++ spl/src/genScen.py
@@ -5,14 +5,6 @@ BENCH_MARK_FOLDER="../bench_mark/"
 MAP_NAME="empty-3-3"
 MAP_HEIGHT=3
 MAP_WIDTH=3
-DEBUG_PERIOD=5 # Seconds
-
-# Global variables
-time_last_debug=0
-time_start=0
-time_end=0
-debug_count=0
-debug_total=0
 
 def get_optimal_path_length(start:tuple[int,int], end:tuple[int,int]) -> int:
     # start:(0,0), end:(2,3)
@@ -24,7 +16,7 @@ def get_optimal_path_length(start:tuple[int,int], end:tuple[int,int]) -> int:
     length += abs(delta_x - delta_y)
     return length
 
-def get_vertices_random(starts, ends) -> tuple[tuple[int,int], tuple[int,int]]:
+def get_instances_random(starts, ends) -> tuple[tuple[int,int], tuple[int,int]]:
     """
     Reassigns source and target vertices randomly until a non-trivial pair is found. 
     """
@@ -42,35 +34,114 @@ def get_vertices_random(starts, ends) -> tuple[tuple[int,int], tuple[int,int]]:
 
         return (start, end)
 
+def get_instances_all() -> tuple[tuple[tuple[int,int], tuple[int,int]]]:
+    vertices = (
+        (0,0), (1,0), (2,0), 
+        (0,1), (1,1), (2,1), 
+        (0,2), (1,2), (2,2), 
+    )
+    
+    perm=[] # 362,880
+    for v in itertools.permutations(vertices, None):
+        perm.append(v)
+
+    non_trivial=set() # 39,630
+    trivial=set() # 97
+    for p in perm[1:]: # Skip trivial instance where source == target for all agents
+
+        # Remove trivial instance where source == target for any agent
+        if any(vertices[i] == p[i] for i in range(len(vertices))):
+            t = tuple(vertices[i] == p[i] for i in range(len(vertices)))
+
+            if t in trivial:
+                continue 
+
+            if rotate_90_clockwise(t) in trivial:
+                continue
+
+            if rotate_90_anticlockwise(t) in trivial:
+                continue
+
+            if rotate_180(t) in trivial:
+                continue
+
+            t_mirror = mirror(t)
+
+            if t_mirror in trivial:
+                continue 
+
+            if rotate_90_clockwise(t_mirror) in trivial:
+                continue
+
+            if rotate_90_anticlockwise(t_mirror) in trivial:
+                continue
+
+            if rotate_180(t_mirror) in trivial:
+                continue
+
+            trivial.add(t)
+
+        if p in non_trivial:
+            continue
+
+        if rotate_90_clockwise(p) in non_trivial:
+            continue
+
+        if rotate_90_anticlockwise(p) in non_trivial:
+            continue
+
+        if rotate_180(p) in non_trivial:
+            continue
+
+        p_mirror = mirror(p)
+
+        if p_mirror in non_trivial:
+            continue
+
+        if rotate_90_clockwise(p_mirror) in non_trivial:
+            continue
+
+        if rotate_90_anticlockwise(p_mirror) in non_trivial:
+            continue
+
+        if rotate_180(p_mirror) in non_trivial:
+            continue
+
+        non_trivial.add(p)
+
+    pairs=[]
+    for nt in non_trivial:
+        pairs.append((vertices, nt))
+    
+    return tuple(pairs)
+
 def create_scen() -> None:
     pathlib.Path(f"./{MAP_NAME}").mkdir(exist_ok=True)
 
     scen_type="random"
-    type_id=2
-    scen_file=f"./{MAP_NAME}/{MAP_NAME}-{scen_type}-{type_id}.scen"
-    starts=[]
-    ends=[]
-    lines=["version 1"]
+    instances=get_instances_all()
 
-    for _ in range(MAP_HEIGHT * MAP_WIDTH):
-        start, end = get_vertices_random(starts, ends)
-        starts.append(start)
-        ends.append(end)
-        length = get_optimal_path_length(start, end)
-        lines.append("\t".join((
-            str(int(length//4)),
-            f"{MAP_NAME}.map",
-            str(MAP_HEIGHT),
-            str(MAP_WIDTH),
-            str(start[0]),
-            str(start[1]),
-            str(end[0]),
-            str(end[1]),
-            "{0:.8f}".format(length),
-        )))
-
-    with open(scen_file, "w") as f:
-        [print(l,file=f) for l in lines]
+    for type_id in range(len(instances)):
+        scen_file=f"./{MAP_NAME}/{MAP_NAME}-{scen_type}-{type_id}.scen"
+        lines=["version 1"]
+        instance = instances[type_id]
+        for i in range(len(instance[0])):
+            start, end = (instance[0][i], instance[1][i])
+            length = get_optimal_path_length(start, end)
+            lines.append("\t".join((
+                str(int(length//4)),
+                f"{MAP_NAME}.map",
+                str(MAP_HEIGHT),
+                str(MAP_WIDTH),
+                str(start[0]),
+                str(start[1]),
+                str(end[0]),
+                str(end[1]),
+                "{0:.8f}".format(length),
+            )))
+            
+        with open(scen_file, "w") as f:
+            [print(l,file=f) for l in lines]
 
 def print_nested(xs):
     size = int(math.sqrt(len(xs)))
@@ -150,86 +221,9 @@ def mirror(xs):
     return tuple(ys)
 
 if __name__ == "__main__":
-    # create_scen()
+    create_scen()
     # debug.draw_paths(MAP_HEIGHT, MAP_WIDTH)
-    vertices = (
-        (0,0), (1,0), (2,0), 
-        (0,1), (1,1), (2,1), 
-        (0,2), (1,2), (2,2), 
-    )
-    # vertices = (
-    #     'a', 'b', 'c',
-    # )
-    perm=[]
-    for v in itertools.permutations(vertices, None):
-        perm.append(v)
-    # print(perm)
-    print(len(perm)) # 362,880
-
-    non_trivial=set()
-    trivial=set()
-    # try:
-    for p in perm[1:]: # Skip trivial instance where source == target for all agents
-
-        # Remove trivial instance where source == target for any agent
-        if any(vertices[i] == p[i] for i in range(len(vertices))):
-            t = tuple(vertices[i] == p[i] for i in range(len(vertices)))
-            if rotate_90_clockwise(t) in trivial:
-                continue
-
-            if rotate_90_anticlockwise(t) in trivial:
-                continue
-
-            if rotate_180(t) in trivial:
-                continue
-
-            t = mirror(t)
-
-            if rotate_90_clockwise(t) in trivial:
-                continue
-
-            if rotate_90_anticlockwise(t) in trivial:
-                continue
-
-            if rotate_180(t) in trivial:
-                continue
-
-            trivial.add(t)
-            non_trivial.add(p)
-            continue
-
-        if rotate_90_clockwise(p) in non_trivial:
-            continue
-
-        if rotate_90_anticlockwise(p) in non_trivial:
-            continue
-
-        if rotate_180(p) in non_trivial:
-            continue
-
-        p = mirror(p)
-
-        if rotate_90_clockwise(p) in non_trivial:
-            continue
-
-        if rotate_90_anticlockwise(p) in non_trivial:
-            continue
-
-        if rotate_180(p) in non_trivial:
-            continue
-
-        non_trivial.add(p)
-    # except KeyboardInterrupt:
-    #     pass
-
-    print(len(non_trivial)) # 65,090
-    print(len(trivial)) # 133
-
-    pairs=[]
-    for nt in non_trivial:
-        pairs.append((vertices, nt))
     
-    # print(pairs)
 
 
 
